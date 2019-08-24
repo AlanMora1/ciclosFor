7d79164838e9c1d6f0ef88b262cad6e6bd0a7235
AlanMora1 <54071972+AlanMora1@users.noreply.github.com>
2019-08-24 15:24:25 -0500

    Add files via upload

---
+++ ciclofor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ciclo_For_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Los numeros del A hasta Z para comenzar la numeracion");
+            Console.WriteLine("De comienza el ciclo (X): ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Donde terminamos el ciclo (Z): ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto deseas que valla el programa: ");
+            z = Convert.ToInt32(Console.ReadLine());
+            if (x > y)
+            {
+                for (int i = x; i > y; i -= z)
+
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+
+            else
+            {
+                for (int i = x; i < y; i += z)
+
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+
+        }
+    }
+}
